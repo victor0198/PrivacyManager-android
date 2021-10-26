11d4a9ddbf490b7b3dad511f968b094798077a5b
C. Victor <victor0198@gmail.com>
2021-10-26 15:53:06 +0300

    handle exit from main menu

---
+++ app/src/main/java/passwordmanager/android/UI/dialogs/ConfirmExit.java
@@ -0,0 +1,33 @@
+package passwordmanager.android.UI.dialogs;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+
+public class ConfirmExit extends DialogFragment {
+    private static String EXIT_MESSAGE = "You want to exit the app?";
+    private static String POSITIVE_MESSAGE = "Exit";
+    private static String NEGATIVE_MESSAGE = "Cancel";
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(EXIT_MESSAGE)
+                .setPositiveButton(POSITIVE_MESSAGE, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton(NEGATIVE_MESSAGE, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+
+        return builder.create();
+    }
+}
+
--- app/src/main/java/passwordmanager/android/UI/menu/MainActivity.java
+++ app/src/main/java/passwordmanager/android/UI/menu/MainActivity.java
@@ -1,8 +1,9 @@
-package passwordmanager.android;
+package passwordmanager.android.UI.menu;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.app.Activity;
 import android.content.Context;
@@ -11,7 +12,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import passwordmanager.android.R;
 import passwordmanager.android.UI.credentials.CredentialsUI;
+import passwordmanager.android.UI.dialogs.ConfirmExit;
 import passwordmanager.android.UI.login.LoginUI;
 import passwordmanager.android.UI.register.RegisterUI;
 import passwordmanager.android.data.account.SharedPreferencesEditor;
@@ -25,10 +28,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginUI.class);
-        launchAuthentication.launch(intent);
+        authenticate();
 
         setListeners();
+    }
+
+    @Override
+    public void onBackPressed() {
+        ConfirmExit confirmExit = new ConfirmExit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        confirmExit.show(fragmentManager, "exitDialog");
+    }
+
+    private void authenticate() {
+        Intent intent = new Intent(this, LoginUI.class);
+        launchAuthentication.launch(intent);
     }
 
     private void setListeners() {
@@ -53,6 +67,10 @@ public class MainActivity extends AppCompatActivity {
                     welcomingText.setText("Hi ".
                             concat(username)
                     );
+                }else{
+                    Log.d(TAG, "StartAuthForResult() :: result -> skipped");
+                    this.finish();
+                    System.exit(0);
                 }
             });
 
