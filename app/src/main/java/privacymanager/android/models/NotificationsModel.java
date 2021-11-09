9ba5dcd442c8b9a5d1458dbcd53c75057fb3999e
C. Victor <victor0198@gmail.com>
2021-11-09 18:36:09 +0200

    storing notifications

--- app/src/main/java/privacymanager/android/models/NotificationsModel.java
+++ app/src/main/java/privacymanager/android/models/NotificationsModel.java
@@ -6,4 +6,55 @@ public class NotificationsModel {
     private long receiverId;
     private String publicKey;
     private String status;
+
+    public void setCreatedRequestId(long createdRequestId) {
+        this.createdRequestId = createdRequestId;
+    }
+
+    public void setSenderId(long senderId) {
+        this.senderId = senderId;
+    }
+
+    public void setReceiverId(long receiverId) {
+        this.receiverId = receiverId;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public long getCreatedRequestId() {
+        return createdRequestId;
+    }
+
+    public long getSenderId() {
+        return senderId;
+    }
+
+    public long getReceiverId() {
+        return receiverId;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationsModel{" +
+                "createdRequestId=" + createdRequestId +
+                ", senderId=" + senderId +
+                ", receiverId=" + receiverId +
+                ", publicKey='" + publicKey + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
